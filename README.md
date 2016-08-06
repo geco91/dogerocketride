06fd7661565e548299ac7a744c3a5c1da8c5a930
rootzoll <christian@geektank.de>
2016-08-06 15:21:38 +0100

    initial somit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dogerocketride
-simple app for the emf2016 badge TILDA MK3
+
+Simple app for the emf2016 badge TILDA MK3 ... feel free to improve it :)
---
+++ main.py
@@ -0,0 +1,22 @@
+import ugfx
+import pyb
+import buttons
+
+def display1():
+	ugfx.display_image(0,0,'apps/dogerocketride/1.gif')
+
+def display2():
+	ugfx.display_image(0,0,'apps/dogerocketride/2.gif')
+
+def display3():
+	ugfx.display_image(0,0,'apps/dogerocketride/3.gif')
+
+buttons.init()
+
+while not buttons.is_pressed("BTN_MENU"):
+	display1()
+	pyb.delay(500)
+	display2()
+	pyb.delay(500)
+	display3()
+	pyb.delay(500)
